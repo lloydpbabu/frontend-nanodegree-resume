0407c6baac336b1c280b45afd7c9bee2c8a40d34
Lloyd P Babu <Lloyd P Babu>
2016-09-18 18:22:46 +0530

    Replace function tested using console

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var awesomeThoughts;
- awesomeThoughts= "Iam Lloyd and I am awesome!";
- console.log(awesomeThoughts);
+ //var awesomeThoughts;
+ //awesomeThoughts= "Iam Lloyd and I am awesome!";
+ //console.log(awesomeThoughts);
+ var email="cameron@udacity.com";
+ var newEmail=email.replace("udacity","gmail");
+ console.log(email);
+ console.log(newEmail);
