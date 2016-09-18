fccb21bb8e6655f97ab1a07b8c7807feb36409e1
Lloyd P Babu <Lloyd P Babu>
2016-09-18 18:04:11 +0530

    Iam awesome text added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var firstName="james";
- var age=32;
- console.log(firstName);
+ var awesomeThoughts;
+ awesomeThoughts= "Iam Lloyd and I am awesome!";
+ console.log(awesomeThoughts);
